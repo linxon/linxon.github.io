bab31d7b0a00b7892946f2953e08d8861a28debb
Yury Martynov <email@linxon.ru>
2024-05-11 09:39:00 +0300

    minor chages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-const d = new Date();
-const github_api_uri = 'https://api.github.com/users/linxon'
+const curr_d = new Date();
+
+const login = 'linxon'
+const github_api_uri = 'https://api.github.com/users/' + login
 
 var getJSON = function(url, callback) {
 	var xhr = new XMLHttpRequest();
+
 	xhr.open('GET', url, true);
 	xhr.responseType = 'json';
 	xhr.onload = function() {
@@ -13,19 +16,76 @@ var getJSON = function(url, callback) {
 			callback(status, xhr.response);
 		}
 	};
+
 	xhr.send();
 };
 
-getJSON(github_api_uri,
-	function(err, data) {
-	if (err !== null) {
-		console.log('Something went wrong: ' + err);
-	} else {
-		document.getElementById("user_avatar").src = data.avatar_url;
-		document.getElementById("user_name").innerHTML = data.name;
-		document.getElementById("user_nickname").innerHTML = '@' + data.login;
-		document.getElementById("user_status").innerHTML = data.bio;
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Strict";
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
 	}
-	
-	document.getElementById("copy_year").innerHTML = d.getFullYear();
-});
+
+	return "";
+}
+
+function checkCookie(target_c, value) {
+	let res = getCookie(target_c);
+	if (res == value) {
+		return true;
+	}
+
+	return false;
+}
+
+function updateHTML_field() {
+	document.getElementById("user_avatar").src = getCookie('avatar_url');
+	document.getElementById("user_name").innerHTML = getCookie('name');
+	document.getElementById("user_nickname").innerHTML = '@' + getCookie('nickname');
+	document.getElementById("user_status").innerHTML = getCookie('status');
+}
+
+function fillWebpage() {
+	if (checkCookie('cookie_stat', '1') == false) {
+		getJSON(github_api_uri,
+			function(err, data) {
+				if (err !== null) {
+					console.log('Something went wrong: ' + err);
+				} else {
+					setCookie("avatar_url", data.avatar_url, 365);
+					setCookie("name", data.name, 365);
+					setCookie("nickname", data.login, 365);
+					setCookie("status", data.bio, 7);
+					setCookie("cookie_stat", '1', 7);
+
+					updateHTML_field();
+				}
+			});
+	} else {
+		updateHTML_field();
+	}
+
+	document.getElementById("copy_year").innerHTML = curr_d.getFullYear();
+}
+
+fillWebpage();
